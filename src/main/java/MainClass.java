890bdea66cb652af36209878b4100bcdfcf719ec
annapanina90 <23510050a>
2018-04-19 11:34:32 +0300

    calculatorOOP

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,104 @@
+import calculator.Calculator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainClass {
+    public static void cls() {
+        for (int i = 0; i <= 25; i++)
+            System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Calculator calc = new Calculator();
+
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Welcome to calculator");
+        String key="";
+        do
+        {
+            // cls();
+            System.out.print("\tChoose operation:");
+            System.out.print("\tX - exit");
+            System.out.print("\tH - help\n");
+
+            try {
+                key = r.readLine().trim().toLowerCase();
+            } catch (NumberFormatException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            calc.setError(false);
+            if ("x".equals( key )) {
+                System.out.println( "Exit" );
+                System.out.println( "Good bye!!!" );
+                r.close();
+                return;
+            } else if ("h".equals( key )) {
+                cls();
+                System.out.println( "+\n-\n*\n/\n" );
+            } else if ("+".equals( key )) {
+                calc.setOperation( '+' );
+                calc.setNumber1( getNumber() );
+                calc.setNumber2( getNumber() );
+                calc.sum();
+            } else if ("-".equals( key )) {
+                calc.setOperation( '-' );
+                calc.setNumber1( getNumber() );
+                calc.setNumber2( getNumber() );
+                calc.minus();
+            } else if ("*".equals( key )) {
+                calc.setOperation( '*' );
+                calc.setNumber1( getNumber() );
+                calc.setNumber2( getNumber() );
+                calc.multi();
+            } else if ("/".equals( key )) {
+                calc.setOperation( '/' );
+                calc.setNumber1( getNumber() );
+                calc.setNumber2( getNumber() );
+                calc.div();
+            } else if ("=".equals( key )) {
+                switch (calc.getOperation()) {
+                    case '+':
+                        calc.sum();
+                    case '-':
+                        calc.minus();
+                    case '*':
+                        calc.multi();
+                    case '/':
+                        calc.div();
+                }
+            } else {
+                calc.setError( true );
+                System.out.println( "Unsupported command!" );
+            }
+            System.out.println( calc.isError() ? "Error" : "Result: " + calc.getNumber1() );
+
+        } while (true);
+
+
+    }
+
+    public static double getNumber() throws IOException
+    {
+        System.out.println("Input number");
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        double number = 0;
+        try {
+            number = Double.parseDouble(r.readLine().trim());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return number;
+    }
+
+}
